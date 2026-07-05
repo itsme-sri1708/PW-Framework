9e6f18c5e014e8a53e89eb17699bb504e826f7c2
itsme-sri1708 <vinoth.sri@gmail.com>
2026-07-05 23:49:04 +0530

    report

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
+import { on } from 'cluster';
 
 /**
  * Read environment variables from file.
@@ -23,14 +24,22 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['allure-playwright']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    actionTimeout:30000,
+    video: 'on',
+    screenshot:'on',
+    trace:'on'
+    
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+   // trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
